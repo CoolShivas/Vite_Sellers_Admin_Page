52f38c8e719943daf1d956114ab9e9d09d42b645
CoolShivas <shivas2710cool00@gmail.com>
2024-04-07 07:17:15 +0530

    Starting_Phase

---
+++ Seller_Admin_Page_with_3Category_SubSection/src/App.jsx
@@ -0,0 +1,11 @@
+import AppName from "./components/AppName";
+
+
+
+function App() {
+  return <>
+    <AppName></AppName>
+  </>
+}
+
+export default App;
---
+++ Seller_Admin_Page_with_3Category_SubSection/src/components/AppName.jsx
@@ -0,0 +1,8 @@
+const AppName = () => {
+    return <center>
+        <h1> Seller_Admin_Page_with_3Category_SubSection </h1>
+    </center>
+}
+
+
+export default AppName;
