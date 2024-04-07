c453e0230009840205ea6855ad422f2486189979
CoolShivas <shivas2710cool00@gmail.com>
2024-04-07 09:11:29 +0530

    Input_Form_Events_Booked

--- Seller_Admin_Page_with_3Category_SubSection/src/components/InputForm.jsx
+++ Seller_Admin_Page_with_3Category_SubSection/src/components/InputForm.jsx
@@ -1,25 +1,61 @@
+import { useState } from "react";
+
 const InputForm = () => {
+
+    const [newProduct, setNewProduct] = useState({
+        proId: '',
+        soldPrice: '',
+        proName: '',
+        caty: 'Electronics',
+    })
+
+    const handlerOnChange = (event) => {
+        console.log(event.target.value);
+    }
+
+    const handleOnSubmit = (event) => {
+        event.preventDefalt()
+        console.log('order booked')
+    }
     return <>
-        <form action="#">
+        <form onSubmit={handleOnSubmit}>
 
             <label htmlFor="productId"> Product Id </label>
-            <input type="text" name="productId" id="productId" />
+            <input type="text"
+                name="productId"
+                id="productId"
+                onChange={handlerOnChange}
+                value={newProduct.productId}
+            />
 
             <label htmlFor="productName"> Product Name </label>
-            <input type="text" name="productName" id="productName" />
+            <input type="text"
+                name="productName"
+                id="productName"
+                onChange={handlerOnChange}
+                value={newProduct.productName}
+            />
 
             <label htmlFor="productPrice"> Product Price </label>
-            <input type="number" name="productPrice" id="productPrice" />
+            <input type="number"
+                name="productPrice"
+                id="productPrice"
+                onChange={handlerOnChange}
+                value={newProduct.productPrice}
+            />
 
             <label htmlFor="chooseCategory"> Choose Category </label>
-            <select name="chooseCategory" id="chooseCategory">
-                <option value="none"> Nome </option>
+            <select name="chooseCategory" id="chooseCategory"
+                onChange={handlerOnChange}
+                value={newProduct.chooseCategory}
+            >
+                <option value="none"> None </option>
                 <option value="Electronics"> Electronics </option>
                 <option value="FoodItems"> FoodItems </option>
                 <option value="SkinCare"> SkinCare </option>
             </select>
 
-            <button> Add Product </button>
+            <button type="submit"> Add Product </button>
         </form>
     </>
 }
